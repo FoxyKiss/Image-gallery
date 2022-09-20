9591eafbbd30d5ca0a4356635a615feeee5213a4
Arseniy Kravtsov <foxkiss98@gmail.com>
2022-09-20 19:56:56 +0600

    feat: Выбрать активный начальный слайд

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin(slideActive) {
+    const slides = document.querySelectorAll('.slide')
 
+    slides[slideActive].classList.add('active')
 
-slides.forEach((slide) => {
-    slide.addEventListener('click', (evt) => {
-    if(!slide.classList.contains('active')) {
-        clearActiveClasses()
-    }
-
-     slide.classList.toggle('active')
-    })
-})
-
-function clearActiveClasses () {
     slides.forEach((slide) => {
-        slide.classList.remove('active')
+        slide.addEventListener('click', (evt) => {
+            if (!slide.classList.contains('active')) {
+                clearActiveClasses()
+            }
+
+            slide.classList.toggle('active')
+        })
     })
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
+
+slidesPlugin(0)
