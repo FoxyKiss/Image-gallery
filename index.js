cf8e2a2d7c85ef59befaa55a66fecc8d01cafd45
Arseniy Kravtsov <foxkiss98@gmail.com>
2022-09-20 20:01:16 +0600

    feat: Написать коментарии к Js коду

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function slidesPlugin(slideActive) {
+    //? Массив слайдов
     const slides = document.querySelectorAll('.slide')
-
+    //? Выбор активного слайда
     slides[slideActive].classList.add('active')
-
-    slides.forEach((slide) => {
+    //? Добавление обработчиков
+        slides.forEach((slide) => {
         slide.addEventListener('click', (evt) => {
             if (!slide.classList.contains('active')) {
                 clearActiveClasses()
@@ -12,7 +13,7 @@ function slidesPlugin(slideActive) {
             slide.classList.toggle('active')
         })
     })
-
+    //?  Удаление классов у слайдов
     function clearActiveClasses() {
         slides.forEach((slide) => {
             slide.classList.remove('active')
