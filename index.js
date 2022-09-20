eda2d2a3919f61a90e60ebbf21a8f8a0d8f6d93f
Arseniy Kravtsov <foxkiss98@gmail.com>
2022-09-20 21:13:27 +0600

    feat: Добавить возможность рендеринга разных марок

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-function slidesPlugin(slideActive) {
-    //? Массив слайдов
-    const slides = document.querySelectorAll('.slide')
-    //? Выбор активного слайда
-    slides[slideActive].classList.add('active')
-    //? Добавление обработчиков
-        slides.forEach((slide) => {
-        slide.addEventListener('click', (evt) => {
-            if (!slide.classList.contains('active')) {
+import { bmwList } from './utils/images.js'
+
+//? Контейнер для слайдов
+const slidesContainer = document.querySelector('.container')
+
+//? Функция получения элемента через Template
+function getTemplate() {
+    const slidesTemplate = document.querySelector('.slides-template')
+    const slideElement = slidesTemplate
+        .content
+        .querySelector('.slide')
+        .cloneNode(true)
+    return slideElement
+}
+
+//? Функция для отображения слайдов
+function slidesPlugin(slidesList, pageTitle) {
+    //? Изменяем главный заголовок
+    const pageTitle = document.querySelector('.title').textContent = pageTitle
+    //? Перебираем массив слайдов
+    slidesList.map((slide) => {
+        const slideElement = getTemplate()
+        slideElement.style = `background-image: url(${slide.image});`
+        slideElement.querySelector('.slide__title').textContent = slide.title
+        //? Вешаем обработчики событий
+        slideElement.addEventListener('click', () => {
+            if (!slideElement.classList.contains('active')) {
                 clearActiveClasses()
             }
-
-            slide.classList.toggle('active')
+            slideElement.classList.toggle('active')
         })
+        //? Добавляем слайды в контейнер
+        slidesContainer.append(slideElement)
     })
-    //?  Удаление классов у слайдов
+
+    //? Функция очищения классов у слайдов
     function clearActiveClasses() {
+        const slides = document.querySelectorAll('.slide')
         slides.forEach((slide) => {
             slide.classList.remove('active')
         })
     }
 }
 
-slidesPlugin(0)
+slidesPlugin(bmwList)
---
+++ utils/images.js
@@ -0,0 +1,25 @@
+const bmwList =
+    [
+        {
+            image: 'https://images.unsplash.com/photo-1638869389391-40c8be6dc010?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80',
+            title: 'M3'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1515574012038-691898bd8f94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+            title: 'M4'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1563969105338-abc74a72e7e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+            title: 'M5'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1625510416933-b758b040443b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
+            title: 'M6'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1548087713-b1fdffcfddff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+            title: 'X7'
+        },
+    ]
+
+    export {bmwList}
